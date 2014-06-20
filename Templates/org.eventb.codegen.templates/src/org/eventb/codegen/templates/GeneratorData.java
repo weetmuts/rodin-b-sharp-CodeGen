e7d7b8692d117f2eccc1d6de4bf78cbe3c0664e5
andy <andyed_2003@yahoo.co.uk>
2014-06-20 17:08:06 +0100

    Added GeneratorData Class, which can be extended with more concrete fields if required.

---
+++ Templates/org.eventb.codegen.templates/src/org/eventb/codegen/templates/GeneratorData.java
@@ -0,0 +1,23 @@
+package org.eventb.codegen.templates;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eventb.codegen.templates.IGeneratorData;
+
+// A Generator Data Object:
+// This is a class holding information to be used for translation.
+// The class can be extended to add fields for holding un-packed data.
+// For example, in FMU translation the translation manager will usually 
+// be needed, so we add a field for it. 
+// The data list is more dynamic, and will contain different objects
+// depending on the translation being undertaken.
+public class GeneratorData implements IGeneratorData {
+	
+	private List<Object> dataList = new ArrayList<Object>();
+	
+	public List<Object> getDataList() {
+		return dataList;
+	}
+
+}
